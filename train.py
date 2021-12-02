0cbe742c9ba81499d9bc32d345969fb46c1b95d5
箫疯 <mxf164419@alibaba-inc.com>
2021-12-02 22:35:04 +0800

    fix batch_size & add lr scaling schme

--- train.py
+++ train.py
@@ -58,9 +58,10 @@ def main():
         else:
             args = args_densenet
         assert args['epochs'] <= 200
-        # force the batch_size to 256, and scaling the lr
-        args['optimizer_hyperparameters']['lr'] *= 256/args['batch_size']
-        args['batch_size'] = 256
+        if args['batch_size'] > 256:
+            # force the batch_size to 256, and scaling the lr
+            args['optimizer_hyperparameters']['lr'] *= 256/args['batch_size']
+            args['batch_size'] = 256
         # Data
         transform_train = transforms.Compose([
             transforms.RandomCrop(32, padding=4),
